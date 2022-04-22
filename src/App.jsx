b0b9f3a5b44a2efe55a1067eba931295949faa0a
moraes <moraeslua66@gmail.com>
2022-04-22 14:55:38 -0300

    build: adiciona style components e configura GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Search from './pages/Search';
 import PlanetsProvider from './context/PlanetsProvider';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
     <PlanetsProvider>
+      <GlobalStyle />
       <Search />
     </PlanetsProvider>
   );
